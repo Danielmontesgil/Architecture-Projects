b4e2651e009c9694ddc23f62e6d45290a88eed3d
Daniel Montes Gil <dmontesgil@gmail.com>
2025-04-24 22:37:33 +0200

    Rewards info changed to string type

--- Assets/Projects/MissionsService/Scripts/IRewardable.cs
+++ Assets/Projects/MissionsService/Scripts/IRewardable.cs
@@ -5,9 +5,9 @@ namespace MissionsService
     public interface IRewardable
     {
         // Probar si podemos saber que recompensa es, solo retornar la interfaz, lo dudo
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
-        public Sprite Icon { get; set; }
-        public float Amount { get; set; }
+        public string Icon { get; set; }
+        public int Amount { get; set; }
     }
 }
